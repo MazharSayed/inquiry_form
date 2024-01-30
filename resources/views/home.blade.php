d3a482b4b5f3dbe35784bf8215b2719956fe06de
MazharSayed <31034876+MazharSayed@users.noreply.github.com>
2024-01-30 15:08:42 +0530

    redirection url added on card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,31 @@
+
 @extends('layouts.adminlayout')
 
 @section('content')
 
 <div class="row">
     <div class="col-lg-3 col-md-6">
-        <div class="card">
-            <div class="card-body">
-                <div class="stat-widget-five">
-                    <div class="stat-icon dib flat-color-1">
-                        <i class="fa fa-users"></i>
-                    </div>
-                    <div class="stat-content">
-                        <div class="text-left dib">
-                            <div class="stat-text">
-                                <span class="count">
-                                    {{@App\Models\Inquiry::count()}}
-                                </span>
+        <a href="{{ route('inquiry.list')}}">
+            <div class="card">
+                <div class="card-body">
+                    <div class="stat-widget-five">
+                        <div class="stat-icon dib flat-color-1">
+                            <i class="fa fa-users"></i>
+                        </div>
+                        <div class="stat-content">
+                            <div class="text-left dib">
+                                <div class="stat-text">
+                                    <span class="count">
+                                        {{@App\Models\Inquiry::count()}}
+                                    </span>
+                                </div>
+                                <div class="stat-heading">Inquiry Users</div>
                             </div>
-                            <div class="stat-heading">Inquiry Users</div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </a>
     </div>
 </div>
 @endsection
